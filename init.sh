401c837dcd68aa9498b7bcdbb66a27d1f563fcb7
Glitch (go-on-glitch) <none>
2019-07-11 15:13:48 +0000

    👓🍕 Checkpoint ./glitch.json:4776115/335 ./.gitignore:4776115/4 ./init.sh:4776115/336

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export GOPATH=$PWD
+export GO111MODULE=on
+if [! -d ~/go]; then
+  cd /tmp
+  wget -q https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
+  tar -xzf go1.12.7.linux-amd64.tar.gz
+  mv go ~
+]
+cd ~/src
+OPENTELEMETRY_LIB=~/stderr.so ~/go/bin/go run main.go
