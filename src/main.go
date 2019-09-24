217017f20bb54db2b9325b205324a528dda9fe76
Glitch (equable-burn) <none>
2019-09-24 18:28:22 +0000

    🏡🎬 Checkpoint ./src/main.go:6431924/23 ./init.sh:6431924/100

--- src/main.go
+++ src/main.go
@@ -57,7 +57,6 @@ func rootHandler(w http.ResponseWriter, req *http.Request) {
 	req = req.WithContext(tag.WithMap(req.Context(), tag.NewMap(tag.MapUpdate{
 		MultiKV: tags,
 	})))
-  fmt.Println(spanCtx)
 
 	ctx, span := tracer.Start(
 		req.Context(),
