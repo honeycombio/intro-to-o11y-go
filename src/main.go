b0446ccc2867e5527f09ff19008182a509b8d7e8
Glitch (equable-burn) <none>
2019-10-31 09:00:17 +0000

    🐠⛵️ Checkpoint ./src/main.go:39666/51 ./velocity-eu-2019-452e0032a2cb.json:39666/2359

--- src/main.go
+++ src/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/honeycombio/opentelemetry-exporter-go/honeycomb"
 	"go.opentelemetry.io/exporter/trace/jaeger"
+  "go.opentelemetry.io/exoprter/trace/stackdriver"
 	sdktrace "go.opentelemetry.io/sdk/trace"
 	"google.golang.org/grpc/codes"
 
